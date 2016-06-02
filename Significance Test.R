a4b31bfc6b5a67d6336ddb2b09bc99267e696f77
Anuj Modi <anuj1modi@gmail.com>
2016-06-01 22:03:49 -0400

    Work done 6/1/16

---
+++ Significance Test.R
@@ -0,0 +1,16 @@
+responses <- read.csv("responses.csv")
+
+lm(data = responses)
+responses.lm <- lm(data = responses)
+summary.lm(responses.lm)
+
+plot(responses,
+     xlab = "Hours Spent on Mobile Device or PC",
+     ylab = "Hours Spent Asleep",
+     main = "Hours Spent on Mobile Device or PC Vs. Hours Spent Asleep",
+     col = "blue",
+     pch = 19)
+
+correlation <- cor(responses$Hours.on.PC.or.Mobile.Device, responses$Hours.Asleep)
+regression <- lm(responses)
+abline(regression)
